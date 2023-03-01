c85c45f916049e6c28f88dccd8cb135b30f34fb3
doug-moore <dougiemooredm63@gmail.com>
2023-03-01 16:00:12 -0500

    Make getSize feature

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 
+
+
+
+// //populate board
+function populate(size){
+    let board = document.querySelector('.board');
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    
+    let numDivs = size * size;
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement('div');
+        board.insertAdjacentElement('beforeend', div);//first arg string, second var
+    }
+
+}
+
+populate(32)
+
+
+//make change size input function
+function getSize(){
+    let input = prompt('Give Me My Size');
+    let message = document.querySelector('#message')
+    if (input == '' || input > 100 || input <1){
+        message.innerHTML = ('Please Enter a Number 1-100')
+    } else {
+        populate(input)
+    }
+}
+// const inputBox = document.querySelector('input-box');
+// inputBox.addEventListener('input', function(){
+
+// })
+
+
+//color square function
+
+
+
+//reset board function
+
+//button event listeners
+
